11b58b1dab82eaea34536cae294cc23711530c39
Rosalyn Land <rosalynl@apadmi.com>
2022-07-29 17:05:39 +0100

    Add user info and button

--- app/src/main/java/com/rosalynland/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/rosalynland/jetbizcard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rosalynland.jetbizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -9,18 +10,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rosalynland.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,8 +62,36 @@ fun CreateBizCard() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(onClick = {
+                    Log.d("Clicked", "CreateBizCard: Clicked")
+                }) {
+                    Text(text = "Portfolio",
+                    style = MaterialTheme.typography.button)
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(horizontalAlignment = Alignment.Start) {
+        Text(
+            text = "Miles P.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant,
+        )
+        Text(
+            text = "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            "@theMilesCompose",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
     }
 }
 
